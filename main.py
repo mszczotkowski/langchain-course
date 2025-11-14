bd4cb0ec6664d1e518537d57809e167031094823
Marek Szczotkowski <marek.szczotkowski@questin.pl>
2025-11-14 21:54:21 +0100

    imports

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+load_dotenv()
+
 def main():
     print("Hello from langchain-course!")
 
